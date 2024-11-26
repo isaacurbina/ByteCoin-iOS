726062d72f9d387737f4efe614d997e1cdfad569
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-11-25 22:37:05 -0600

    169. Step 5: Update the User Interface Using the Delegate Pattern (#3)

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -9,25 +9,25 @@
 import Foundation
 
 protocol CoinManagerDelegate {
-	func didGetResponse(_ weather: CoinApiResponse)
+	func didGetResponse(_ data: CoinData)
 	func didFailWithError(_ error: Error)
 }
 
 struct CoinManager {
 	
 	private let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-	private let apiKey = "669365E4-555E-40D4-8953-3303C1680CFB"
+	private let apiKey = "F73EC27B-6438-498F-9C83-0F97E7C99139"
 	var delegate: CoinManagerDelegate?
 	
 	let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 	
 	func getCoinPrice(for currency: String) {
 		let requestUrl = "\(baseURL)/\(currency)?apiKey=\(apiKey)"
-		print(requestUrl)
-		performRequest(with: requestUrl)
+		print(requestUrl, currency)
+		performRequest(with: requestUrl, currency: currency)
 	}
 	
-	private func performRequest(with urlString: String) {
+	private func performRequest(with urlString: String, currency: String) {
 		if let url = URL(string: urlString) {
 			let session = URLSession(configuration: .default)
 			let task = session.dataTask(with: url) { (data, response, error) in
@@ -37,7 +37,11 @@ struct CoinManager {
 				}
 				if let safeData = data {
 					if let coinApiResponse = self.parseJSON(safeData) {
-						self.delegate?.didGetResponse(coinApiResponse)
+						let coinData = CoinData(
+							rate: String(format: "%.2f", coinApiResponse.rate!),
+							currency: currency
+						)
+						self.delegate?.didGetResponse(coinData)
 					}
 				}
 			}
@@ -45,10 +49,10 @@ struct CoinManager {
 		}
 	}
 	
-	private func parseJSON(_ apiResponse: Data) -> CoinApiResponse? {
+	private func parseJSON(_ data: Data) -> CoinApiResponse? {
 		let decoder = JSONDecoder()
 		do {
-			let decodedData = try decoder.decode(CoinApiResponse.self, from: apiResponse)
+			let decodedData = try decoder.decode(CoinApiResponse.self, from: data)
 			return decodedData
 		} catch {
 			self.delegate?.didFailWithError(error)
